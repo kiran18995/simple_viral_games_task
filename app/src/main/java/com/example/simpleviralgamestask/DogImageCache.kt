5d6b3b430dc2746e486491832be786b43f5b12e8
Kiran kumar N <kiraninbox18995@gmail.com>
2023-07-19 16:50:54 +0530

    resolved to show recent order first

--- app/src/main/java/com/example/simpleviralgamestask/DogImageCache.kt
+++ app/src/main/java/com/example/simpleviralgamestask/DogImageCache.kt
@@ -1,30 +1,32 @@
 package com.example.simpleviralgamestask
 
-import android.content.Context
-import android.content.SharedPreferences
 import android.util.LruCache
-import com.google.gson.Gson
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.util.LinkedList
 
 object DogImageCache {
 
     private const val MAX_CACHE_SIZE = 20
     private val dogImageCache = LruCache<String, DogImageResponse>(MAX_CACHE_SIZE)
+    private val cachedDogImageKeys = LinkedList<String>()
 
     fun cacheDogImage(dogImage: DogImageResponse) {
         dogImageCache.put(dogImage.message, dogImage)
+        cachedDogImageKeys.remove(dogImage.message)
+        cachedDogImageKeys.addFirst(dogImage.message)
     }
 
     fun getAllCachedDogImages(): List<DogImageResponse> {
-        val snapshot = dogImageCache.snapshot()
-        return snapshot.values.toList()
+        val cachedDogImages = mutableListOf<DogImageResponse>()
+        for (key in cachedDogImageKeys) {
+            dogImageCache.get(key)?.let {
+                cachedDogImages.add(it)
+            }
+        }
+        return cachedDogImages
     }
 
     fun clearCache() {
         dogImageCache.evictAll()
+        cachedDogImageKeys.clear()
     }
 }
